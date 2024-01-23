e1e50b8dce3bbce001873f09520c32a93f0fd3b3
Thats-Neat <john.rohweder1015@gmail.com>
2024-01-22 22:48:29 -0800

    works in some cases

--- src/wagner/algo.py
+++ src/wagner/algo.py
@@ -1,37 +1,36 @@
 
 class wagner_fischer():
-    
+
     def __init__(self):
         pass
-    
+
     def manual_input(self):
         word = input("Misspelled Word: ")
         possible = input("Possible Word: ")
         return word, possible
-    
+
     def wagner(self):
         word, possible = self.manual_input()
-        
+
         matrix = []
         matrix = [[] for _ in range(len(possible) + 1)]
-        
-        
+
         for row in range(0, len(word) + 1):
             matrix[0].append(row)
-            
+
         for column in range(1, len(word) + 1):
             matrix[column].append(column)
-        
-        
-        
-        
-        
+
+        for row in range(1, len(matrix)):
+            for column in range(1, len(word) + 1):
+
+                tri = [matrix[row][column - 1], matrix[row - 1]
+                       [column], matrix[row - 1][column - 1]]
+
+                if word[column - 1] == possible[row - 1]:
+                    matrix[row].append(min(tri))
+                else:
+                    matrix[row].append(min(tri) + 1)
+
         for row in matrix:
-            print(row) 
-        
-        
-        
-        
-        
-        
-        
+            print(row)
