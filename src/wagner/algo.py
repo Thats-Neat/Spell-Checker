bc4f02c27151703cfe3355b1d9db125f234748e7
Thats-Neat <john.rohweder1015@gmail.com>
2024-01-22 22:12:30 -0800

    matrix starter

--- src/wagner/algo.py
+++ src/wagner/algo.py
@@ -4,5 +4,34 @@ class wagner_fischer():
     def __init__(self):
         pass
     
+    def manual_input(self):
+        word = input("Misspelled Word: ")
+        possible = input("Possible Word: ")
+        return word, possible
+    
     def wagner(self):
-        pass
+        word, possible = self.manual_input()
+        
+        matrix = []
+        matrix = [[] for _ in range(len(possible) + 1)]
+        
+        
+        for row in range(0, len(word) + 1):
+            matrix[0].append(row)
+            
+        for column in range(1, len(word) + 1):
+            matrix[column].append(column)
+        
+        
+        
+        
+        
+        for row in matrix:
+            print(row) 
+        
+        
+        
+        
+        
+        
+        
